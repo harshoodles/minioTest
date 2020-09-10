242dccaf553ae7698a4db6ee2fc8d00af87f5302
Harsh Kumar <harsh@harsh.kumar>
2020-09-10 11:46:37 +0530

    modified script

--- file-downloader.py
+++ file-downloader.py
@@ -1,4 +1,8 @@
-from minio import Minio from minio.error import ResponseError import sys
+# Import MinIO library.
+from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+import sys
 
 # Initialize minioClient with an endpoint and access/secret keys.
 minioClient = Minio('localhost:9000',
@@ -9,7 +13,7 @@ n = len(sys.argv)
 for i in range(1, n):
 # Get a full object
  try:
-      minioClient.fget_object('bucket01', sys.argv[i], '/home/harsh/snap/%s' %sys.argv[i] )
+      minioClient.fget_object('bucket01', sys.argv[i], '/home/harsh/snap/minIO/%s' %sys.argv[i] )
  except ResponseError as err:
       print(err)
 
--- loop.sh
+++ loop.sh
@@ -1,8 +1,23 @@
 #! /bin/bash
 
-python list-bucket.py > output1.txt
+#python list-bucket.py > output1.txt
 
-data=( $(grep . output1.txt |cut -d= -f2) )
+#data=( $(grep . output1.txt |cut -d= -f2) )
 
-echo $data
-python list-object.py $data
+
+python list-object.py bucket01 > output2.txt
+
+filename=output2.txt
+while read -r line; do
+    name="$line"
+  python file-downloader.py $name
+
+done < "$filename"
+
+filename=output2.txt
+while read -r line; do
+    name="$line"
+  python file-uploader.py $name
+
+done < "$filename"
+
